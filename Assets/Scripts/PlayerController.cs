a792a20d671603848b321e67d9b51236514923a7
Hauwoong <k55mdjyp9m@privaterelay.appleid.com>
2026-01-19 00:15:32 +0900

    overlap기법과 Raycast기법 구현및 Gizmos를 사용하여 디버깅

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,15 +3,32 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 1.0f;
-    public float JumpForce = 1.0f;
-    Rigidbody2D rb;
+    [Header("Movement Details")]
+    [SerializeField] public float speed = 1.0f;
+    [SerializeField] public float JumpForce = 1.0f;
+
+    [Header("Collision Details")]
+    // Overlap기법을 사용하여 착지 유무 확인
+    //[SerializeField] private Transform groundcheck;
+    //[SerializeField] private float groundRadius = 0.15f;
+
+    //Raycast기법을 사용하여 착지 유무 확인
+    [SerializeField] private float RayDistance = 0.1f;
+
+    //땅 레이어 설정
+    [SerializeField] private LayerMask groundLayer;
+
     Vector2 input;
     bool JumpPressed;
+
+    private Animator animator;
+    private Rigidbody2D rb;
+    [SerializeField] private bool RightFacing = true;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponentInChildren<Animator>();
     }
 
     void Update()
@@ -31,16 +48,82 @@ public class PlayerController : MonoBehaviour
 
     void FixedUpdate()
     {
+        HandleFlip();
+
         rb.linearVelocity = new Vector2(input.x * speed, rb.linearVelocityY);
 
-        if (JumpPressed && IsGrouded()) {
+        if (JumpPressed && IsGrounded_Raycast()) {
             rb.linearVelocity = new Vector2(rb.linearVelocityX, JumpForce);
         }
 
         JumpPressed = false;
+
+        HandleAnimator();
+    }
+    // overlap기법
+    /*
+    bool IsGrounded_Overlap () {
+        return Physics2D.OverlapCircle(
+                groundcheck.position,
+                groundRadius,
+                groundLayer
+            );
+    }
+    
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(groundcheck.position, groundRadius);
+    }
+    */
+
+
+    // Raycast기법
+    private bool IsGrounded_Raycast() {
+        Collider2D col = GetComponent<Collider2D>();
+        if(col == null) return false;
+
+        Vector2 origin = col.bounds.center;
+        origin.y = col.bounds.min.y + 0.01f;
+
+        RaycastHit2D hit = Physics2D.Raycast(origin, Vector2.down, RayDistance, groundLayer);
+
+        return hit.collider != null;
     }
 
-    bool IsGrouded() {
-        return Mathf.Abs(rb.linearVelocity.y) < 0.01f;
+    private void OnDrawGizmosSelected()
+    {
+        Collider2D col = GetComponent<Collider2D>();
+        if (col == null) return;
+
+        Gizmos.color = Color.red;
+
+        Vector2 origin = col.bounds.center;
+        origin.y = col.bounds.min.y + 0.01f;
+
+        Gizmos.DrawLine(origin, origin + Vector2.down * RayDistance);
     }
+
+    private void HandleAnimator() {
+        bool isMoving = Mathf.Abs(rb.linearVelocityX) > 0.01f;
+
+        animator.SetBool("isMoving", isMoving);
+    }
+
+    private void HandleFlip() {
+        if (rb.linearVelocityX > 0 && !RightFacing)
+        {
+            Filp();
+        }
+        else if (rb.linearVelocityX < 0 && RightFacing){
+            Filp();
+        }
+    }
+
+    private void Filp() {
+        transform.Rotate(0, 180, 0);
+        RightFacing = !RightFacing;
+    }
+
+    
 }
